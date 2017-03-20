e32755339c344d4a9e322ffa235db74259422f0a
DrunkPony <drunk@pony.com>
2017-03-20 22:47:18 +0100

    update script to point new software

--- software/tools/orca_compile.sh
+++ software/tools/orca_compile.sh
@@ -9,13 +9,14 @@ export PATH=$PATH:/opt/altera16.1_lite/quartus/bin/
 
 
 
-cd ../orca/software/de2-test
+cd ../orca/software/generic
+make clean
 make
 
 cd ../../tools/
 rm de2-test.elf.qex
-./elf2hex.sh ../software/de2-test/de2-test.elf
-cp de2-test.elf.qex ../../HPS_FPGA_LED/fpga/test.hex
+./elf2hex.sh ../software/generic/orca.elf
+cp orca.elf.qex ../../HPS_FPGA_LED/fpga/test.hex
 quartus_cdb --update_mif ../..//HPS_FPGA_LED/fpga/HPSFPGA.qpf
 quartus_asm ../..//HPS_FPGA_LED/fpga/HPSFPGA.qpf
 quartus_cpf -c -o bitstream_compression=on ../..//HPS_FPGA_LED/fpga/output_files/HPSFPGA.sof ../..//HPS_FPGA_LED/fpga/output_files/soc_system.rbf
