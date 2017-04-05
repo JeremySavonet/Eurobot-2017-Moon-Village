dc0d841ab0e155802b8fab1487e22cb595a7b470
jeremySavonet <jeremy.savonet@gmail.com>
2017-04-05 17:56:09 +0200

    AStar: Add shared library for strategy

---
+++ software/astar/include/WestBot/AStar/MapNode.hpp
@@ -0,0 +1,39 @@
+// Copyright (c) 2016-2017 All Rights Reserved WestBot
+
+#ifndef WESTBOT_ASTAR_MAPNODE_HPP_
+#define WESTBOT_ASTAR_MAPNODE_HPP_
+
+#include <QGraphicsPixmapItem>
+#include <QList>
+#include <QPair>
+
+#include "Export.hpp"
+
+#include "Utils.hpp"
+
+namespace WestBot {
+namespace AStar {
+
+class WESTBOT_ASTAR_EXPORT MapNode
+{
+public:
+    NodeType type;
+
+    uint expandCost;
+
+    QGraphicsPixmapItem pixmapItem;
+};
+
+struct WESTBOT_ASTAR_EXPORT RunResources
+{
+    class MapNode** map;
+    QList< QPair< uint, uint> >* path;
+    QList< QList< NodeState > >* steps;
+    QPair< uint, uint > start;
+    QPair< uint, uint > end;
+};
+
+}
+}
+
+#endif // WESTBOT_ASTAR_MAPNODE_HPP_
