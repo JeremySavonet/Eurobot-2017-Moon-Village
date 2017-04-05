d4259b9994df90451112540caafc58dfcff069f7
JeremySavonet <jeremy.savonet@gmail.com>
2017-04-05 20:27:33 +0200

    HELLO WORLD FOR BRICE

--- software/mainboard_high_level/src/main.cpp
+++ software/mainboard_high_level/src/main.cpp
@@ -1,5 +1,7 @@
 // Copyright (c) 2016-2017 All Rights Reserved WestBot
 
+#define SIMU
+
 #include <QCoreApplication>
 #include <QDebug>
 #include <QHostAddress>
@@ -7,8 +9,12 @@
 #include <QNetworkInterface>
 #include <QThread>
 
+#include <hps_arm.h> // For our base address
+
 #include <Defines.hpp>
 
+#include <WestBot/Input.hpp>
+#include <WestBot/Output.hpp>
 #include <WestBot/Configuration.hpp>
 #include <WestBot/ConfigurationTcpServer.hpp>
 #include <WestBot/Hal.hpp>
@@ -79,6 +85,8 @@ int main( int argc, char *argv[] )
     Hal hal;
     hal.init(); // Open the FPGA memory and load our module
 
+    MemoryManager memoryManager;
+
     // Init the System manager
     SystemManager system( hal );
     system.init(); // Create the state machine
@@ -86,9 +94,95 @@ int main( int argc, char *argv[] )
     // Here we are BITCHESSSSSS !!!
     system.start(); // Start the state machine
 
+    // Create our layer mapping
+    Memory layer1( memoryManager, PIO_N_LAYER1_BASE );
+    Memory layer2( memoryManager, PIO_N_LAYER2_BASE );
+    Memory layer3( memoryManager, PIO_N_LAYER3_BASE );
+
+    // Read layer 1 registers
+    qDebug() << "Layer 1 registers:";
+    qDebug() << "==================";
+
+    for( int i = 0; i < 64; ++i )
+    {
+        qDebug()
+            << "Read register" << i << ":"
+            << QString::number( layer1.read( i * 4, 32 ), 16 );
+    }
+
+    // Read layer 2 registers
+    qDebug() << "Layer 2 registers:";
+    qDebug() << "==================";
+
+    for( int i = 0; i < 64; ++i )
+    {
+        qDebug()
+            << "Read register" << i << ":"
+            << QString::number( layer2.read( i * 4, 32 ), 16 );
+    }
+
+    // Read layer 3 registers
+    qDebug() << "Layer 3 registers:";
+    qDebug() << "==================";
+
+    for( int i = 0; i < 64; ++i )
+    {
+        qDebug()
+            << "Read register" << i << ":"
+            << QString::number( layer3.read( i * 4, 32 ), 16 );
+    }
+
+#ifdef SIMU
+    ItemRegister configStateRegister(
+        layer1,
+        1 * 4,
+        32 );
+
+    configStateRegister.write( 0xffffffff );
+#endif
+
+    // INPUT REGISTER
+    ItemRegister::Ptr inputRegister1 = std::make_shared< ItemRegister >( layer1, 7 * 4, 8 );
+    ItemRegister::Ptr inputRegister2 = std::make_shared< ItemRegister >( layer1, 7 * 4 + 1, 8 );
+    ItemRegister::Ptr inputRegister3 = std::make_shared< ItemRegister >( layer1, 7 * 4 + 2, 8 );
+
+    // OUTOUT REGISTER OVERRIDE
+    ItemRegister outputOverride( layer1, 10 * 4, 32 );
+    outputOverride.write( 0x01010101 );
+
+    ItemRegister::Ptr outputLedRegister = std::make_shared< ItemRegister >( layer1, 9 * 4, 8 );
+    ItemRegister::Ptr outputIO1Register = std::make_shared< ItemRegister >( layer1, 9 * 4 + 1, 8 );
+    ItemRegister::Ptr outputIO2Register = std::make_shared< ItemRegister >( layer1, 9 * 4 + 2, 8 );
+    ItemRegister::Ptr outputIO3Register = std::make_shared< ItemRegister >( layer1, 9 * 4 + 3, 8 );
+
+    // Buttons
+    Input tirette( inputRegister1, "Tirette" );
+    Input color( inputRegister2, "Color" );
+    Input arretUrgence ( inputRegister3, "AU" );
+
+    // Leds
+    Output led( outputLedRegister, "Led" );
+    Output io1( outputIO1Register, "IO1" );
+    Output io2( outputIO2Register, "IO2" );
+    Output io3( outputIO3Register, "IO3" );
+
     while( 1 )
     {
-        QThread::msleep( 100 ); // Idle
+        qDebug() << "Tirette" << tirette.digitalRead();
+        qDebug() << "Color" << color.digitalRead();
+        qDebug() << "AU" << arretUrgence.digitalRead();
+
+        led.digitalWrite( DigitalValue::ON );
+        io1.digitalWrite( DigitalValue::ON );
+        io2.digitalWrite( DigitalValue::OFF );
+        io3.digitalWrite( DigitalValue::OFF );
+        QThread::msleep( 250 );
+        led.digitalWrite( DigitalValue::OFF );
+        io1.digitalWrite( DigitalValue::OFF );
+        io2.digitalWrite( DigitalValue::ON );
+        io3.digitalWrite( DigitalValue::ON );
+        QThread::msleep( 250 );
+
         QCoreApplication::processEvents();
     }
 
