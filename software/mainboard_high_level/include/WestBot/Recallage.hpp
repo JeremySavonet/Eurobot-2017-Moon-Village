b402ea1e101f43235f635e132b4f44dbf0f26f02
JeremySavonet <jeremy.savonet@gmail.com>
2017-05-24 10:20:25 +0200

    Update recallage module

--- software/mainboard_high_level/include/WestBot/Recallage.hpp
+++ software/mainboard_high_level/include/WestBot/Recallage.hpp
@@ -8,6 +8,12 @@
 namespace WestBot
 {
 
+typedef struct {
+    double x;
+    double y;
+    double theta;
+} RobotPos;
+
 class Hal;
 
 class Recallage
@@ -15,19 +21,52 @@ class Recallage
 public:
     Recallage();
 
-    void init( Hal& hal );
+    bool init( Hal& hal );
+
+    void errorInit( double errX, double errY, double errTheta );
+
+    void errorModify( double errX, double errY, double errTheta );
+
+    RobotPos calibrate(
+        int len,
+        const double* mesR,
+        const double* mesTheta );
+
+    RobotPos getPos(); // Unused for now
+    RobotPos sendPos( const RobotPos& robotPos );
 
 private:
+    /*
     float* _theta;
     float* _distance;
     int _lenght;
     float _odoTheta;
     float _odoX;
     float _odoY;
-
+    */
     ItemRegister::Ptr _odoThetaReg;
     ItemRegister::Ptr _odoXReg;
     ItemRegister::Ptr _odoYReg;
+    bool _attached;
+
+    RobotPos error = { 0, 0, 0 };
+
+    struct {
+        bool dir;
+        double	ax;
+        double	ay;
+        double	bx;
+        double	by;
+    } tableBorder[6] = {
+        { 0,      0, -1500,      0,      1500 },
+        { 1,      0,  1500,   2000,      1500 },
+        { 0,   2000,  1500,   2000,     -1500 },
+        { 1,   2000, -1500,      0,     -1500 },
+        { 0, 360+22, -1500, 360+22, -1500+710 },
+        { 0, 360+22,  1500, 360+22,  1500-710 },
+    };
+
+    int tableBorderNb = sizeof( tableBorder ) / sizeof( tableBorder[ 0 ] );
 };
 
 }
